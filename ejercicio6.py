53cac45454a00da3063af3eade00a6f5fc705fdc
BurciagaGovea <rodrigovea0@gmail.com>
2024-05-26 17:18:45 -0600

    Programa 6 - Número mayor

---
+++ ejercicio6.py
@@ -0,0 +1,9 @@
+Num1 = float(input("Ingresa el número 1: "))
+Num2 = float(input("Ingresa el número 2: "))
+
+if Num1 > Num2: 
+    print(f"El número mayor es el número 1: {Num1}")
+elif Num1 == Num2:
+    print(f"Los dos números son iguales")
+else:
+    print(f"El número mayor es el segundo: {Num2}")
