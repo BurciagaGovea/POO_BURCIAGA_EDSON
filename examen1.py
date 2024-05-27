d9511a31081075959c2267e4355de5352846b8eb
BurciagaGovea <rodrigovea0@gmail.com>
2024-05-27 10:43:13 -0600

    Examen parcial 1 - 1.1

---
+++ examen1.py
@@ -0,0 +1,7 @@
+numero = float(input(f"Ingrese un número: "))
+if numero > 0:
+    print(f"{numero} es positivo")
+elif numero == 0:
+    print(f"{numero} es cero")
+else:
+    print(f"{numero} es negativo")
