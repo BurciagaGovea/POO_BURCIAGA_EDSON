f245e9fcc210fa76f75cb253b61a55731d3dddcb
BurciagaGovea <rodrigovea0@gmail.com>
2024-07-02 22:16:11 -0600

    Programa 21 - Tupla desempaquetada

---
+++ ejercicio21.py
@@ -0,0 +1,6 @@
+datos = (
+    "Edson", "Masculino", 18, "Mexicano", "Estudiante"
+)
+
+Nombre, Sexo, Edad, Nacionalidad, Ocupación = datos
+print(f"Nombre: {Nombre} \nSexo: {Sexo}\nEdad: {Edad}\nNacionalidad: {Nacionalidad}\nOcupación: {Ocupación}")
